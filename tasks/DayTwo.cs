2a404fad75f8dcb31969e0cf2e4507f43b07a137
huineith <westholm.jacob@gmail.com>
2024-12-10 09:09:24 +0100

    fixed special case that was not handled correctly

--- tasks/DayTwo.cs
+++ tasks/DayTwo.cs
@@ -1,3 +1,5 @@
+using System.Diagnostics;
+
 namespace tasks;
 
 public class DayTwo
@@ -7,31 +9,214 @@ public class DayTwo
         
     }
 
-    public int ProblemOne()
+    public void ProblemOne(string file)
     {
-        var data =SimpleFunctions.GetIntData("d2_p1.txt") ;
+        var data =SimpleFunctions.GetIntData(file) ;
 
-     
-        
-        return -1; 
+        int validReports = 0;
+        int validityCode;
+         
+        foreach (List<int> report in data)
+        {
+            validityCode = checkReportValidity(report); 
+            if (validityCode<0)
+            { 
+                validReports += 1; 
+            }
+            
+        }
+
+
+        Console.WriteLine(validReports);
+         
     }
 
 
-    public bool CheckStepVality(int a, int b, bool increasing )
+    public void ProblemTwo(string file)
     {
+        var data = SimpleFunctions.GetIntData(file);
 
-        bool valid; 
-        if (increasing)
+        int validReports = 0;
+        int validityCode;
+
+
+        int Index = -1;
+        foreach (List<int> report in data)
         {
-            valid = (b > a) && (b - a) < 4; 
-        }
-        else
-        {
-            valid= !(b > a) &&   (b-a) > 4 ;  
-        }
+            Index += 1;
+            validityCode = checkReportValidity(report);
+          
+            if (validityCode < 0)
+            {
+                validReports += 1;
+                
+            }
+            else
+            {
 
-        return valid; 
+                if (validityCode == 1)
+                {
+                    var specialCase1 = contractList(report, validityCode);
+                    var specialCase2 = contractList(report, validityCode+1);
+                    var specialCase3 = contractList(report, validityCode-1); // due to possiblity of changing increasing or decreasing makeing (report[1],report[2]) valid 
+                    
+                    if (checkReportValidity(specialCase1) < 0 || checkReportValidity(specialCase2) < 0 ||checkReportValidity(specialCase3) < 0 )
+                    {
+                        validReports += 1;
+                    }
+                }
+                else
+                {
+                    var altList1 = contractList(report, validityCode);
+                    var altList2 = contractList(report, validityCode+1);
 
+                    if (checkReportValidity(altList1) < 0 || checkReportValidity(altList2) < 0)
+                    {
+                        validReports += 1;
+                    }
+
+                }
+               
+                
+                
+            }
+
+           
+        }
+        Console.WriteLine(validReports);
     }
+
+    #region ReportLogic
     
+        
+       public int checkReportValidity(List<int> report)
+        { 
+            int i;
+            bool valid=true;
+            bool increasing;
+            int invalidIndex;
+
+           
+        
+            if (report[0] == report[1])
+            {
+                invalidIndex = 0;
+            }
+            else
+            {
+                invalidIndex = -1;
+                increasing = report[1] > report[0]; 
+                for(  i=0; i<report.Count-1; i++ )
+                {
+                    valid = CheckStepVality(report[i], report[i + 1], increasing);
+                    if (!valid)
+                    {
+                        invalidIndex = i;
+                        break; 
+                    }
+                }
+            }
+            return invalidIndex; 
+
+        }
+       
+       
+       
+        public int checkReportValidityV2(List<int> report)
+        { 
+            int i;
+            bool valid=true;
+            bool increasing;
+            int invalidIndex;
+
+            if (report.Count == 0)
+            {
+                return 0; 
+            }
+        
+            if (report[0] == report[1] && report[0]==report[2]  )
+            {
+                    invalidIndex = 0;    
+                
+            }
+            else
+            {
+                if (report[0] == report[1])
+                {
+                    increasing = report[2] > report[0]; 
+                }
+                else
+                {
+                    increasing = report[1] > report[0]; 
+                }
+                invalidIndex = -1;
+               
+                for(  i=0; i<report.Count-2; i+=2)
+                {
+                    valid = CheckStepVality(report[i], report[i + 1],report[i+2], increasing);
+                    if (!valid)
+                    {
+                        invalidIndex = i;
+                        break; 
+                    }
+                }
+
+                if (valid)
+                {
+                    valid = CheckStepVality(report[report.Count - 2], report[report.Count - 1], increasing);
+                    if (!valid)
+                    {
+                        invalidIndex = report.Count;
+                    }
+                }
+                
+            }
+            return invalidIndex; 
+
+        }
+    
+      
+        public bool CheckStepVality(int a, int b, bool increasing )
+        {
+
+            bool valid; 
+            if (increasing)
+            {
+                valid = (b > a) && (b - a) < 4; 
+            }
+            else
+            {
+                valid= (a > b) &&   (b-a) > -4  ;  
+            }
+
+            return valid; 
+
+        }
+        
+        
+        
+        public bool CheckStepVality(int a, int b,int c, bool increasing )
+        {
+
+            bool valid = CheckStepVality(a, b, increasing);
+            if (!valid)
+            {
+                if (CheckStepVality(a, c, increasing) || CheckStepVality(b, c, increasing))
+                {
+                    valid = true; 
+                }
+                
+            }
+
+            return valid;
+        }
+        
+        #endregion
+
+        public List<int> contractList(List<int> report, int index)
+        {
+            var newList= report.GetRange(0,index) ;
+            newList.AddRange(report.GetRange(index+1, report.Count-(index+1)));
+            return newList;
+        }
 }
--- tasks/Program.cs
+++ tasks/Program.cs
@@ -7,6 +7,17 @@ using System.Collections;
 // day1.FirstProblem();
 // day1.SecondProblem();
 var day2 = new DayTwo();
-day2.ProblemOne();
+// day2.ProblemOne("d2_p1.txt");
+day2.ProblemTwo("d2_p1.txt");
 
+// var myList = new List<int>() {81,82,81,80,79};
+// var i = day2.checkReportValidity(myList);
+// var alt1 = day2.contractList(myList, i);
+// var alt2 = day2.contractList(myList, i+1);
+// //
+// Console.WriteLine(i);
+// Console.WriteLine( SimpleFunctions.intListToString(alt1) +$":{day2.checkReportValidity(alt1)}"   );
+// Console.WriteLine( SimpleFunctions.intListToString(alt2) +$":{day2.checkReportValidity(alt2)}"   );
+// Console.WriteLine((day2.checkReportValidity(alt1) < 0 || day2.checkReportValidity(alt2) < 0));
 
+// Console.WriteLine(SimpleFunctions.intListToString(day2.contractList(myList,0)));
